09a62aa3fd6615432e41bf5bf20d43b47bd1afde
EltonCampos07 <eltonapcnunes@gmail.com>
2025-09-22 01:01:06 +0000

    feat: criacao dos exercicios parte 1 de streams

---
+++ Streams_Collectors_Optional/livros/Produto.java
@@ -0,0 +1,52 @@
+import java.util.Locale;
+
+public class Produto {
+    private Integer codigo;
+    private String nome;
+    private CategoriaProduto categoria;
+    private Double preco;
+
+    public Produto(Integer codigo, String nome, CategoriaProduto categoria, Double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public String toString() {
+        return String.format(Locale.forLanguageTag("pt-BR"), "[%d] %s %s R$ %.2f", codigo, nome, categoria, preco);
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaProduto categoria) {
+        this.categoria = categoria;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+}
