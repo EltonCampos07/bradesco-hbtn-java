26d6b619119bb67e63147665c5a3dad542e452b0
EltonCampos07 <eltonapcnunes@gmail.com>
2025-09-27 13:20:07 +0000

    feat: criacao dos exercicios da parte 3 de stream

---
+++ Streams_Collectors_Optional/preco_minimo/Cliente.java
@@ -0,0 +1,36 @@
+public class Cliente {
+    private Integer codigo;
+    private String nome;
+    private Boolean premium;
+
+    public Cliente(Integer codigo, String nome, Boolean premium) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.premium = premium;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Boolean getPremium() {
+        return premium;
+    }
+
+    public void setPremium(Boolean premium) {
+        this.premium = premium;
+    }
+
+}
---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,24 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class Consulta {
+    
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+            .filter(produto -> produto.getCategoria().equals(CategoriaProduto.LIVRO))
+            .toList();
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream()
+            .sorted(Comparator.comparing(Produto::getPreco).reversed())
+            .findFirst()
+            .get();
+    }
+
+        public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, Integer precoMinimo) {
+            return produtos.stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .toList();
+        }
+}
