ced4fb7f18bd90f980af78ac227a4d7307bc7eef
EltonCampos07 <eltonapcnunes@gmail.com>
2025-09-27 21:50:57 +0000

    feat: finalizacao do exercicio Collectors parte 1

---
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -0,0 +1,62 @@
+import java.util.Locale;
+
+public class Pessoa {
+    private Integer codigo;
+    private String nome;
+    private String cargo;
+    private Integer idade;
+    private Integer salario;
+
+    public Pessoa(Integer codigo, String nome, String cargo, Integer idade, Integer salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Integer getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Integer salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.forLanguageTag("pt-BR"), "[%d] %s %s %s R$ %d", codigo, nome, cargo, idade, salario);
+    }
+}
