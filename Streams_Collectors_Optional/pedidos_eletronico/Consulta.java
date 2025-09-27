154bfde10fbfda5bceec5a714a42106493445174
EltonCampos07 <eltonapcnunes@gmail.com>
2025-09-27 14:17:20 +0000

    feat: criacao dos exercicios parte 4 de stream

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -0,0 +1,31 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria().equals(CategoriaProduto.LIVRO))
+                .toList();
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst()
+                .get();
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, Integer precoMinimo) {
+        return produtos.stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .toList();
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos.stream()
+            .filter(pedido -> pedido.getProdutos().stream()
+                .anyMatch(produto -> produto.getCategoria().equals(CategoriaProduto.ELETRONICO)))
+            .toList();
+    }
+}
