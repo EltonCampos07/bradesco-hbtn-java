78ece38285002a8155956afe5a82d291f0279ae4
EltonCampos07 <eltonapcnunes@gmail.com>
2025-10-03 20:41:23 +0000

    finalizacao do exercicio 1 do Streams_Collector_Optional_Exercicios

---
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -0,0 +1,50 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+    
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+    
+    public int getCodigo() {
+        return codigo;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public String getCargo() {
+        return cargo;
+    }
+    
+    public int getIdade() {
+        return idade;
+    }
+    
+    public double getSalario() {
+        return salario;
+    }
+    
+    @Override
+    public String toString() {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.forLanguageTag("pt-BR"));
+        return String.format("[%d] %s %s %d %s", 
+            codigo, nome, cargo, idade, formatter.format(salario));
+    }
+    
+    @Override
+    public int compareTo(Pessoa other) {
+        return this.nome.compareTo(other.nome);
+    }
+}
