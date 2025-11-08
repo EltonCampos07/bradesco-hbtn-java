b4d141cba7831a65be9b581158ff4522be8b4a16
EltonCampos07 <eltonapcnunes@gmail.com>
2025-11-08 20:57:28 -0300

    feat: criacao dos exercicio de tratamento de erros

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,15 @@
+package divisao;
+
+public class Numero {
+
+    public static void dividir(int a, int b){
+        var resultado = 0;
+        try{
+            resultado = a/b;
+        }catch (Exception ex) {
+            System.out.println("Nao eh possivel dividir por zero");
+        }finally {
+            System.out.printf("%d / %d = %d\n", a, b, resultado);
+        }
+    }
+}
