63f4840661de350a55e6651e88dd6f66838d8ae0
EltonCampos07 <eltonapcnunes@gmail.com>
2025-12-17 22:28:47 -0300

    feat: finalizacao da parte do exercicio de exception

---
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -0,0 +1,14 @@
+package livraria;
+
+import livraria.exceptions.AutorInvalidoException;
+import livraria.exceptions.LivroInvalidoException;
+
+public class LivroEdicaoOuro extends Livro {
+    public LivroEdicaoOuro(String var1, String var2, double var3) throws AutorInvalidoException, LivroInvalidoException {
+        super(var1, var2, var3);
+    }
+
+    public double getPreco() {
+        return super.getPreco() * 1.3;
+    }
+}
---
+++ 0x03/livraria/Program.java
@@ -0,0 +1,43 @@
+package livraria;
+
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] var0) {
+        try {
+            Livro var1 = new Livro("As barbas do imperador: D. Pedro II", "Lilia Moritz Schwarcz", (double)56.0F);
+            System.out.printf(new Locale("pt", "BR"), "Titulo: %s - Autor: %s - Preco: %.2f\n", var1.getTitulo(), var1.getAutor(), var1.getPreco());
+        } catch (Exception var6) {
+            System.out.println(var6.getMessage() + "\n");
+        }
+
+        try {
+            LivroEdicaoOuro var7 = new LivroEdicaoOuro("Xato", "Fernando Morais", 38.3);
+            System.out.printf(new Locale("pt", "BR"), "Titulo: %s - Autor: %s - Preco: %.2f\n", var7.getTitulo(), var7.getAutor(), var7.getPreco());
+        } catch (Exception var5) {
+            System.out.println(var5.getMessage() + "\n");
+        }
+
+        try {
+            LivroEdicaoOuro var8 = new LivroEdicaoOuro("Ladrao de Raios", "Rick", 21.88);
+            System.out.printf(new Locale("pt", "BR"), "Titulo: %s - Autor: %s - Preco: %.2f\n", var8.getTitulo(), var8.getAutor(), var8.getPreco());
+        } catch (Exception var4) {
+            System.out.println(var4.getMessage() + "\n");
+        }
+
+        try {
+            Livro var9 = new Livro("ab", "Jane Doe", 15.99);
+            System.out.printf(new Locale("pt", "BR"), "Titulo: %s - Autor: %s - Preco: %.2f\n", var9.getTitulo(), var9.getAutor(), var9.getPreco());
+        } catch (Exception var3) {
+            System.out.println(var3.getMessage() + "\n");
+        }
+
+        try {
+            Livro var10 = new Livro("Harry Potter e o enigma do principe", "J.K. Rowling", (double)0.0F);
+            System.out.printf(new Locale("pt", "BR"), "Titulo: %s - Autor: %s - Preco: %.2f\n", var10.getTitulo(), var10.getAutor(), var10.getPreco());
+        } catch (Exception var2) {
+            System.out.println(var2.getMessage() + "\n");
+        }
+
+    }
+}
